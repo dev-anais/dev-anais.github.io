401b5895a2568943387816c39ce0aff974459323
Anais.yun <38196875+dev-anais@users.noreply.github.com>
2023-12-18 01:27:00 +0900

    did work for theme

--- src/App.css.ts
+++ src/App.css.ts
@@ -1,5 +1,24 @@
-import { style } from "@vanilla-extract/css";
+import { createTheme, style } from "@vanilla-extract/css";
+
+export const [themeClass, vars] = createTheme({
+  color: {
+    primary: "#0A5C36",
+    white: "#fff",
+  },
+  fontSize: {
+    small: "12px",
+    medium: "16px",
+    large: "20px",
+    xLarge: "24px",
+  },
+});
 
 export const container = style({
-  backgroundColor: "#0A5C36",
+  minHeight: "100vh",
+  backgroundColor: vars.color.primary,
+});
+
+export const Title = style({
+  fontSize: vars.fontSize.xLarge,
+  color: vars.color.white,
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { container } from "./App.css";
+import { Title, container, themeClass } from "./App.css";
 
 function App() {
   return (
-    <div className={container}>
+    <div className={`${themeClass} ${container}`}>
       <header>
-        <h1>Eunhye Anais Yun</h1>
+        <h1 className={Title}>Anais Yun</h1>
+        <h2>Experienced FrontEnd Developer</h2>
+        <h3>
+          I build Web and Mobile applications with JavaScript / React. And I
+          believe programming can make life easier.
+        </h3>
       </header>
       <main></main>
     </div>
